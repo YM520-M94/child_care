c309594062d69bf8023779a6bfe182f65da8a340
YM520-M94 <79740262+YM520-M94@users.noreply.github.com>
2021-07-16 07:31:29 +0900

    recordsコントローラー作成

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,48 @@
 class RecordsController < ApplicationController
+  before_action :set_record, except: [:index, :new, :create]
 
   def index
+    @records = Record.all
+  end
+
+  def new
+    @record = Record.new
+  end
+
+  def create
+    @record = Record.new(record_params)
+    if @record.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @record.update(record_params)
+      redirect_to: record_path(@record)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
     
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:date, :weather, :title, :diary, :image).merge(user_id: current_user.id)
+  end
+  
+  def set_record
+    @record = Record.find(params[:id])
   end
 end
